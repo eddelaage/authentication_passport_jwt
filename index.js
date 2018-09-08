ad2aa65d1b9d6bd15d715632d3ca78c2ce0c5616
Edouard de Laage <edouarddelaage@gmail.com>
2018-09-08 13:05:31 +0200

    Add cors package / end fetch route test with token

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const morgan = require ('morgan')
 require('./passport-strategy')
 const auth = require('./auth')
 const passport = require('passport')
+var cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin)
